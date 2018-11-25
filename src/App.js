d5ee8eeeea169f62c094cce9948ed9c702f2ce2c
UNYEONG <eunnamu094@gmail.com>
2018-11-25 22:13:14 +0900

    fix coding style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 
-class App extends Component {
+class App extends React.Component {
   state = {
     loading: false,
     images: []
